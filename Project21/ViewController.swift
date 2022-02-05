1aa06b6c0c0f6751381a0d8e0a0fd90d0266c907
uzayaltiner <92029942+uzayaltiner@users.noreply.github.com>
2022-02-05 19:37:37 +0300

    added notification

--- Project21/ViewController.swift
+++ Project21/ViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 import UserNotifications
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
         }
     }
     @objc func scheduleLocal(){
+        registerCategories()
         let center = UNUserNotificationCenter.current()
         center.removeAllPendingNotificationRequests()
         let content = UNMutableNotificationContent()
@@ -39,11 +40,39 @@ class ViewController: UIViewController {
         var dateComponents = DateComponents()
         dateComponents.hour = 10
         dateComponents.minute = 30
-       // let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        // let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
         
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         center.add(request)
     }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
+        
+        
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let customData = userInfo["customData"] as? String {
+            print("custom data received: \(customData)")
+            
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                print("Default identifier")
+            case "show":
+                print("Show more information")
+            default:
+                break
+            }
+        }
+        completionHandler()
+    }
 }
-
